765794631b06bdcc361147bfbeabdde1c21c1ef3
Aurélien Revault d'Allonnes <helmet67fr@yahoo.fr>
2020-07-09 10:21:08 +0200

    update

--- index.js
+++ index.js
@@ -4,22 +4,23 @@
 const serviceAccount = require('./serviceAccount.json');
 const firebase = require('./node_modules/firebase-admin');
 const data  = require('./data/data.json'); // put your data here
-const collectionKey = "my_collection/sub_collection/"; //name/path of the collection
-const gDatabaseUrl = "https://<my-project-id>.firebaseio.com"
-const db = firebase.firestore();
+const firestorePath = 'my_collection/sub_document/sub_collection'; //name/path of the collection (must be an even number)
+const gDatabaseUrl = 'https://<your-project-id>.firebaseio.com';
+
 
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: gDatabaseURL,
+  databaseURL: gDatabaseUrl,
 });
 
+const db = firebase.firestore();
 
 function importJSON(jsonData)
 {
 
     jsonData.forEach(item =>{
-        db.collection(collectionKey).add(item).then((res) => {
+        db.collection(firestorePath).add(item).then((res) => {
             console.log("Document successfully written!");
         }).catch((error) => {
             console.error("Error writing document: ", error);
