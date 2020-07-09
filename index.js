a4268768ba42667b8359b4c134be9e8dabca25c8
Aurélien Revault d'Allonnes <helmet67fr@yahoo.fr>
2020-07-09 10:22:07 +0200

    typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const serviceAccount = require('./serviceAccount.json');
 const firebase = require('./node_modules/firebase-admin');
 const data  = require('./data/data.json'); // put your data here
-const firestorePath = 'my_collection/sub_document/sub_collection'; //name/path of the collection (must be an even number)
+const firestorePath = 'my_collection/sub_document/sub_collection'; //name/path of collection (must be an even number)
 const gDatabaseUrl = 'https://<your-project-id>.firebaseio.com';
 
 
